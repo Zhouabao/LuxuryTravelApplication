1001512c0f59ed48698d3530152aa8950971326e
18379630701@163.com <18379630701@163.com>
2021-04-27 17:46:47 +0800

    联系方式未通过提示弹窗位置错误，应在界面最下方

--- app/src/main/java/com/sdy/luxurytravelapplication/ui/dialog/ContactNotPassDialog.kt
+++ app/src/main/java/com/sdy/luxurytravelapplication/ui/dialog/ContactNotPassDialog.kt
@@ -36,9 +36,9 @@ class ContactNotPassDialog : BaseBindingDialog<DialogContactPassBinding>() {
 
     private fun initWindow() {
         val window = this.window
-        window?.setGravity(Gravity.CENTER)
+        window?.setGravity(Gravity.BOTTOM)
         val params = window?.attributes
-        params?.width =WindowManager.LayoutParams.WRAP_CONTENT
+        params?.width =WindowManager.LayoutParams.MATCH_PARENT
         params?.height = WindowManager.LayoutParams.WRAP_CONTENT
         params?.windowAnimations = R.style.MyDialogCenterAnimation
         window?.attributes = params
